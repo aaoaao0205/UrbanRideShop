85f7cce4f64136413166df6875b4517bafc34503
aaoaao0205 <aaoaao0205@gmail.com>
2023-09-22 21:18:05 +0900

    修正

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  # Instance Methods
   def admin?
     admin
   end    
 end
+
