5dd982b34f4058ac86784db7f75f3f68e4f9517d
aaoaao0205 <aaoaao0205@gmail.com>
2023-09-23 21:50:49 +0900

    商品出品機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'items#index'
-  resources :items, only: [:index]
+  resources :items, only: [:index, :new, :create] 
 end
